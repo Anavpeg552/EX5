b44fab5ecb6b837bc689033275df72dd2bdbf003
Anavpeg552 <anavpeg552@g.educaand.es>
2024-06-07 14:27:20 +0200

    v1

--- src/Ut6_1.java
+++ src/Ut6_1.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Ut6_1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        //Set
+        for (int i=10; i>1; i--){
+            int numero;
+            if(numero>0 && numero<=100){
+                System.out.print("Quedan "+i+": "+numero);
+                numero=sc.nextInt();
+            }else{
+                System.out.println("NO VALIDO");
+                i++;
+            }
+
+
+        }
     }
 }
